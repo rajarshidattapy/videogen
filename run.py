568c3bd5b8b0f9a9266ac62e42ef4eab620cb48e
rajashidattapy <rayan05rio@gmail.com>
2026-07-24 05:19:42 +0530

    add run.py to display Python version information

---
+++ run.py
@@ -0,0 +1,8 @@
+import sys
+import platform
+
+# Get a detailed tuple (e.g., major, minor, micro)
+print(sys.version_info)
+
+# Get the version as a clean string (e.g., "3.13.0")
+print(platform.python_version())
